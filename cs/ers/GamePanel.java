00c53b8d8b844176ebee60d1a09b4228fe8f413e
firefly431 <firefly431.awesome@gmail.com>
2014-10-21 13:08:10 -0500

    Copied stuff from tetris

---
+++ cs/ers/GamePanel.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// TODO: MUSIC http://www3.ntu.edu.sg/home/ehchua/programming/java/J8c_PlayingSound.html
+
+package cs.ers;
+
+import java.awt.*;
+import java.awt.event.*;
+
+import java.util.Iterator;
+
+/**
+ *
+ * @author s506571
+ */
+public class GamePanel extends StatePanel {
+    @Override
+    public void paint(Graphics g) {
+        //
+    }
+}
